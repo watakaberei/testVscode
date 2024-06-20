9b67a1093842c1223e3def5009d2e37aa1d1cc80
watakaberei <watakabereirei0502@gmail.com>
2024-06-20 23:03:10 +0900

    初コミット

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website</title>
+</head>
+<body>
+    <h1>Githubにpush</h1>
+</body>
+</html>
